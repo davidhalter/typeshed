3319cadf85012328f8a12b15da4eecc8de0cf305
Dave Halter <davidhalter88@gmail.com>
2019-05-19 00:14:39 +0200

    Make builtins work
    One change is a Jedi issue, Jedi cannot really deal with __new__.
    The other is a typeshed issue. "function" should not be defined in the stubs.

--- stdlib/2and3/builtins.pyi
+++ stdlib/2and3/builtins.pyi
@@ -852,6 +852,7 @@ class slice(object):
 
 class tuple(Sequence[_T_co], Generic[_T_co]):
     def __new__(cls: Type[_T], iterable: Iterable[_T_co] = ...) -> _T: ...
+    def __init__(self, iterable: Iterable[_T_co] = ...): ...
     def __len__(self) -> int: ...
     def __contains__(self, x: object) -> bool: ...
     @overload
@@ -871,15 +872,6 @@ class tuple(Sequence[_T_co], Generic[_T_co]):
         def index(self, x: Any, start: int = ..., end: int = ...) -> int: ...
     else:
         def index(self, x: Any) -> int: ...
-
-class function:
-    # TODO not defined in builtins!
-    __name__: str
-    __module__: str
-    if sys.version_info >= (3,):
-        __qualname__: str
-        __code__: CodeType
-        __annotations__: Dict[str, Any]
 
 class list(MutableSequence[_T], Generic[_T]):
     @overload
